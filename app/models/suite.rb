07d6287ead3e6e31bdec8c0b03466c4d9ca04432
Jimmie Elvenmark <flugsio@gmail.com>
2020-01-29 10:57:38 +0000

    Increase number of runs

--- app/models/suite.rb
+++ app/models/suite.rb
@@ -18,6 +18,6 @@ class Suite < ActiveRecord::Base
   end
 
   def purge_old_runs
-    self.runs.order(id: :desc).offset(5).destroy_all
+    self.runs.order(id: :desc).offset(30).destroy_all
   end
 end
