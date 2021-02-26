bff8718610fef5ed1dd8ed0f6924493cd67e0aad
Jimmie Elvenmark <flugsio@gmail.com>
2021-02-26 13:26:44 +0000

    Optimize compare, change IM to graphicsmagick
    This does not convert the original images to fit them inside a canvas
    size, because it takes ~100ms for imagemagic to do that for each image
    (baseline and test). The new file was previously named .png, which now
    requires a weird workaround, this should be fixable in some other way.
    
    Graphicsmagic does not support the same compare options as imagemagic,
    so this works very different.
    
    Total processing (background job, depends on server load)
    
    Before: ~3000ms
    After: ~600ms

--- app/jobs/process_test_job.rb
+++ app/jobs/process_test_job.rb
@@ -1,9 +1,18 @@
+require 'securerandom'
+
 class ProcessTestJob < ApplicationJob
   queue_as :default
 
   def perform(test_params)
+    # horrible hack to get the correct metadata through dragonfly
+    # otherwise the images aren't opened inline
+    file = test_params[:screenshot]
+    def file.original_filename
+      "#{SecureRandom.alphanumeric}.png"
+    end
     ImageProcessor.crop(test_params[:screenshot].path, test_params[:crop_area]) if test_params[:crop_area]
-    @test = Test.create!(test_params)
+    @test = Test.new(test_params)
+    @test.save!
     ScreenshotComparison.new(@test, test_params[:screenshot])
     @test
   end
--- app/models/screenshot_comparison.rb
+++ app/models/screenshot_comparison.rb
@@ -18,32 +18,19 @@ class ScreenshotComparison
 
   def temp_screenshot_paths(test)
     {
-      baseline: File.join(Rails.root, 'tmp', "#{test.id}_baseline.png"),
-      test: File.join(Rails.root, 'tmp', "#{test.id}_test.png"),
       diff: File.join(Rails.root, 'tmp', "#{test.id}_diff.png")
     }
   end
 
   def compare_images(test, image_paths)
-    canvas = create_canvas(test)
-    baseline_resize_command = convert_image_command(test.screenshot_baseline.path, image_paths[:baseline], canvas.to_h)
-    test_size_command = convert_image_command(test.screenshot.path, image_paths[:test], canvas.to_h)
-    compare_command = compare_images_command(image_paths[:baseline], image_paths[:test], image_paths[:diff], test.fuzz_level, test.highlight_colour)
-    # run all commands in serial
-    Open3.popen3("#{baseline_resize_command} && #{test_size_command} && #{compare_command}") { |_stdin, _stdout, stderr, _wait_thr| stderr.read }
+    compare_command = compare_images_command(test.screenshot_baseline.path, test.screenshot.path, image_paths[:diff], test.fuzz_level, test.highlight_colour)
+    Open3.popen3(compare_command) { |_stdin, stdout, _stderr, _wait_thr| stdout.read.split.map(&:to_f).sum/3 }
   end
 
   def compare_images_command(baseline_file, compare_file, diff_file, fuzz, highlight_colour)
-    "compare -alpha Off -dissimilarity-threshold 1 -fuzz #{fuzz} -metric AE -highlight-color '##{highlight_colour}' #{baseline_file.shellescape} #{compare_file.shellescape} #{diff_file.shellescape}"
+    "gm compare -metric MAE -highlight-color '##{highlight_colour}' -file #{diff_file.shellescape} #{baseline_file.shellescape} #{compare_file.shellescape} | grep -E '(Red|Green|Blue)' | tr -s ' ' | cut -d' ' -f 3"
   end
 
-  def create_canvas(test)
-    # create a canvas using the baseline's dimensions
-    Canvas.new(
-      ImageGeometry.new(test.screenshot_baseline.path),
-      ImageGeometry.new(test.screenshot.path)
-    )
-  end
 
   def determine_baseline_image(test, screenshot)
     # find an existing baseline screenshot for this test
@@ -64,15 +51,9 @@ class ScreenshotComparison
     test.save!
   end
 
-  def convert_image_command(input_file, output_file, canvas)
-    "convert #{input_file.shellescape} -background white -extent #{canvas[:width]}x#{canvas[:height]} #{output_file.shellescape}"
-  end
-
   def determine_pass(test, image_paths, compare_result)
     begin
-      img_size = ImageSize.path(image_paths[:diff]).size.inject(:*)
-      pixel_count = (compare_result.to_f / img_size) * 100
-      test.diff = pixel_count.round(2)
+      test.diff = (compare_result * 100).round(2)
       # TODO: pull out 0.1 (diff threshhold to config variable)
       (test.diff < 0.1)
     rescue
@@ -82,8 +63,6 @@ class ScreenshotComparison
 
   def save_screenshots(test, image_paths)
     # assign temporary images to the test to allow dragonfly to process and persist
-    test.screenshot = Pathname.new(image_paths[:test])
-    test.screenshot_baseline = Pathname.new(image_paths[:baseline])
     test.screenshot_diff = Pathname.new(image_paths[:diff])
     test.save
     test.create_thumbnails
@@ -91,8 +70,6 @@ class ScreenshotComparison
 
   def remove_temp_files(image_paths)
     # remove the temporary files
-    File.delete(image_paths[:test])
-    File.delete(image_paths[:baseline])
     File.delete(image_paths[:diff])
   end
 end
